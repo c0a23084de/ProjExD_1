fa1ee093aae9d45cb02554c88ecc3b12ee4093f6
power suzuki <c0a23084de@edu.teu.ac.jp>
2024-06-11 17:13:40 +0900

    名前変更

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -26,14 +26,22 @@ def main():
         screen.blit(bg_img, [-x+3200, 0]) 
         screen.blit(bg_img2, [-x+4800,0])
         key_lst = pg.key.get_pressed()
-        if key_lst[pg.K_UP]:
-            kk_rct.move_ip(0, -1)
-        if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip(0, 1)
-        if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(1, 0)
-        if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip(-1, 0)
+
+        a = -1
+        b = 0
+        if key_lst[pg.K_UP]: #練習8
+            a = 0
+            b = -1
+        elif key_lst[pg.K_DOWN]:
+            a = 0
+            b = 1
+        elif key_lst[pg.K_RIGHT]:
+            a = 1
+            b = 0
+        elif key_lst[pg.K_LEFT]:
+            a = -2
+            b = 0
+        kk_rct.move_ip((a, b))
         screen.blit(kk_img, kk_rct) #kk_imgをkk_rctの設定に従って貼り付け
         pg.display.update()
         tmr += 1        
